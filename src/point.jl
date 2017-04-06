f7f3550758b33655212febb97d12b36ed91d5005
Tamas Nagy <tamas@tamasnagy.com>
2017-04-06 14:33:52 -0700

    remove more extraneous uses of `@compat`

--- src/point.jl
+++ src/point.jl
@@ -2,19 +2,15 @@
 # Higher-order measures
 
 @compat const Vec{N} = NTuple{N, Measure}
-@compat const Vec2 = Vec{2}
-@compat const Vec3 = Vec{3}
+const Vec2 = Vec{2}
+const Vec3 = Vec{3}
 
 isabsolute(p::Vec) = false
 isabsolute{N}(p::NTuple{N, AbsoluteLength}) = true
 
-#Vec{T <: Length}(x::T, y::T) = (x, y)
-#Vec(x::Measure, y::Measure) = Vec{2, Measure}((x, y))
-#Vec() = Vec(0mm, 0mm)
-
 @compat const AbsoluteVec{N} = NTuple{N, Length{:mm, Float64}}
-@compat const AbsoluteVec2 = AbsoluteVec{2}
-@compat const AbsoluteVec3 = AbsoluteVec{3}
+const AbsoluteVec2 = AbsoluteVec{2}
+const AbsoluteVec3 = AbsoluteVec{3}
 
 #Base.zero(::Type{Vec}) = Vec()
 
